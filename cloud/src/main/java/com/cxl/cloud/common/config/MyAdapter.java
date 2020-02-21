61c354ac69c1f7fdf8244bdea42bf0c9088ed7a1
cxl <1799359457@qq.com>
2020-02-21 14:12:30 +0800

    update

--- cloud/src/main/java/com/cxl/cloud/common/config/MyAdapter.java
+++ cloud/src/main/java/com/cxl/cloud/common/config/MyAdapter.java
@@ -3,6 +3,7 @@ package com.cxl.cloud.common.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class MyAdapter implements WebMvcConfigurer {
     @Override
--- cloud/src/main/java/com/cxl/cloud/common/config/MyWebAppConfigurer.java
+++ cloud/src/main/java/com/cxl/cloud/common/config/MyWebAppConfigurer.java
@@ -3,6 +3,7 @@ package com.cxl.cloud.common.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class MyWebAppConfigurer implements WebMvcConfigurer {
     @Override
