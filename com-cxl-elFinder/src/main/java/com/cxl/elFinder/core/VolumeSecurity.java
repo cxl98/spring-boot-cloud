bb2397ec618ea6a492948f77498c439f5c4a79dc
cxl <1799359457@qq.com>
2020-03-22 13:59:52 +0800

    添加

---
+++ com-cxl-elFinder/src/main/java/com/cxl/elFinder/core/VolumeSecurity.java
@@ -0,0 +1,9 @@
+package com.cxl.elFinder.core;
+
+import com.cxl.elFinder.core.impl.SecurityConstant;
+
+
+public interface VolumeSecurity {
+    String getVolumePattern();
+    SecurityConstant getSecurityConstraint();
+}
