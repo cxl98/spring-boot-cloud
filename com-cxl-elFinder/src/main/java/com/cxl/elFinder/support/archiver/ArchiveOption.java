6040fc1131bfcb8b8ef598d7f5d66aa644b4949d
cxl <1799359457@qq.com>
2020-03-27 22:13:23 +0800

    update

---
+++ com-cxl-elFinder/src/main/java/com/cxl/elFinder/support/archiver/ArchiveOption.java
@@ -0,0 +1,23 @@
+package com.cxl.elFinder.support.archiver;
+
+import com.alibaba.fastjson.JSONObject;
+
+public enum  ArchiveOption {
+    INSTANCE;//实例
+
+    public static JSONObject JSON_INSTANCE(){
+        JSONObject instance=new JSONObject();
+        instance.put("create",getCreate());//创建
+        instance.put("extract",getExtract());//拿
+        return instance;
+    }
+
+    private static String[] getCreate() {
+        return ArchiveType.SUPPORT_MIME_TYPES;
+    }
+
+    private static String[] getExtract() {
+        return ArchiveType.SUPPORT_MIME_TYPES;
+    }
+
+}
