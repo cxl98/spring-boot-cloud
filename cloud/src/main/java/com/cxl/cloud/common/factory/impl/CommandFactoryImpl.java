1feff86bba9e7b14879b0350695038980528368c
cxl <1799359457@qq.com>
2020-04-27 21:34:56 +0800

    添加了文件的操作

---
+++ cloud/src/main/java/com/cxl/cloud/common/factory/impl/CommandFactoryImpl.java
@@ -0,0 +1,43 @@
+package com.cxl.cloud.common.factory.impl;
+
+import com.cxl.cloud.common.factory.CommandFactory;
+import com.cxl.elFinder.command.ElFinderCommand;
+import com.cxl.elFinder.support.concurrency.GenericCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutionException;
+
+public class CommandFactoryImpl implements CommandFactory {
+    private static final Logger LOGGER= LoggerFactory.getLogger(CommandFactory.class);
+
+    private String classNamePattern;
+
+    private final GenericCache<String,ElFinderCommand> cache=new GenericCache<>();
+    @Override
+    public ElFinderCommand get(String commandName) {
+        if (null==commandName||commandName.isEmpty()) {
+            LOGGER.error(String.format("Command %s cannot be null or empty",commandName));
+            throw new RuntimeException(String.format("Command %s cannot be null or empty", commandName));
+        }
+        ElFinderCommand command=null;
+        try {
+            command=cache.getValue(commandName, () -> {
+                LOGGER.debug(String.format("trying recovery command!: %s",commandName));
+                String className=String.format(getClassNamePattern(),commandName.substring(0,1).toUpperCase()+commandName.substring(1));
+                return (ElFinderCommand) Class.forName(className).newInstance();
+            });
+        } catch (ExecutionException | InterruptedException e) {
+            LOGGER.error("Unable to get/create command instance.", e);
+        }
+        return command;
+    }
+
+    private String getClassNamePattern() {
+        return classNamePattern;
+    }
+
+    public void setClassNamePattern(String classNamePattern) {
+        this.classNamePattern = classNamePattern;
+    }
+}
