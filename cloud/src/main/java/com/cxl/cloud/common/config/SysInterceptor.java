3668fd086ad8aa964f49aa4872d55bad6d352bec
cxl <1799359457@qq.com>
2021-02-17 23:20:50 +0800

    update

--- cloud/src/main/java/com/cxl/cloud/common/config/SysInterceptor.java
+++ cloud/src/main/java/com/cxl/cloud/common/config/SysInterceptor.java
@@ -1,10 +1,8 @@
 package com.cxl.cloud.common.config;
 
-<<<<<<< HEAD
+
 import com.cxl.cloud.entry.User;
-=======
-import com.cxl.cloud.entry.SysUser;
->>>>>>> 9e674c949047e857516405bf511d169fd491bbf2
+
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -18,11 +16,8 @@ public class SysInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (handler instanceof HandlerMethod) {
-<<<<<<< HEAD
+
             User user = (User) request.getSession().getAttribute("user");
-=======
-            SysUser user = (SysUser) request.getSession().getAttribute("user");
->>>>>>> 9e674c949047e857516405bf511d169fd491bbf2
             if (user == null) {
                 response.sendRedirect("/");
                 return false;
